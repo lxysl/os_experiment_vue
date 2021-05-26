3ecdf9f2526fe35a44e01b81f0e11320b87600b9
lxy764139720 <764139720@qq.com>
2021-05-27 00:03:18 +0800

    feat: 使用Vue编写前端，处理机、后备队列、挂起队列、挂起/解挂进程 fix: 处理机分配进程算法

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
-    <!--    <img alt="Vue logo" src="./assets/logo.png">-->
     <os msg="操作系统课程设计"></os>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import os from './components/os.vue'
 
 export default {
   name: 'App',
-  props: {
-    msg: String
-  },
   components: {
     // HelloWorld,
     os
--- src/components/os.vue
+++ src/components/os.vue
@@ -3,7 +3,7 @@
     <el-container>
       <el-header>{{ msg }}</el-header>
       <el-container style="height: 500px">
-        <el-aside width="30%" style="padding: 20px">
+        <el-aside width="40%" style="padding: 20px">
           <el-tabs v-model="operationName" type="border-card">
             <el-tab-pane label="创建进程" name="tab_first" style="height: 380px">
               <el-form ref="form" :model="form" label-width="80px">
@@ -43,47 +43,76 @@
                   </el-select>
                 </el-form-item>
               </el-form>
-              <el-button type="primary" @click="submit">立即创建</el-button>
+              <el-button type="primary" @click="createPCB">立即创建</el-button>
             </el-tab-pane>
-            <el-tab-pane label="挂起进程" name="tab_second" style="height: 380px">挂起进程</el-tab-pane>
-            <el-tab-pane label="解挂进程" name="tab_third" style="height: 380px">解挂进程</el-tab-pane>
+            <el-tab-pane label="挂起 / 解挂进程" name="tab_second" style="height: 380px">
+              <div style="text-align: center; padding-top: 40px">
+                <el-transfer
+                    style="text-align: left; display: inline-block"
+                    v-model="hangingValue"
+                    :titles="['未挂起', '挂起']"
+                    :format="{
+                      noChecked: '0/${total}',
+                      hasChecked: '${checked}/${total}'
+                    }"
+                    @change="hangingDataChange"
+                    :data="pcb_value">
+                  <span slot-scope="{ option }">{{ option.key }}</span>
+                </el-transfer>
+              </div>
+            </el-tab-pane>
             <el-tab-pane label="运行" name="tab_forth" style="height: 380px">
               <el-button type="primary" @click="refresh">刷新</el-button>
               <el-button type="primary" @click="run">运行</el-button>
             </el-tab-pane>
           </el-tabs>
         </el-aside>
-        <el-main width="70%">
+        <el-main width="60%">
           <el-table :data="pcbData" border style="width: 100%" height="450">
-            <el-table-column fixed prop="pid" label="进程号" width="80"></el-table-column>
-            <el-table-column fixed prop="time" label="进程所需时间" width="120"></el-table-column>
-            <el-table-column fixed prop="ram" label="进程所需内存" width="120"></el-table-column>
-            <el-table-column fixed prop="priority" label="进程优先级" width="120"></el-table-column>
-            <el-table-column fixed prop="property" label="进程属性" width="120"></el-table-column>
-            <el-table-column fixed prop="precursor" label="进程前驱" width="200"></el-table-column>
-            <el-table-column fixed prop="successor" label="进程后继" width="200"></el-table-column>
+            <el-table-column fixed prop="pid" label="进程号" width="80" align="center"></el-table-column>
+            <el-table-column prop="time" label="进程所需时间" width="120" align="center"></el-table-column>
+            <el-table-column prop="ram" label="进程所需内存" width="120" align="center"></el-table-column>
+            <el-table-column prop="priority" label="进程优先级" width="120" align="center"></el-table-column>
+            <el-table-column prop="state" label="进程状态" width="120" align="center"></el-table-column>
+            <el-table-column prop="property" label="进程属性" width="120" align="center"></el-table-column>
+            <el-table-column prop="precursor" label="进程前驱" width="200" align="center"></el-table-column>
+            <el-table-column prop="successor" label="进程后继" width="200" align="center"></el-table-column>
           </el-table>
         </el-main>
       </el-container>
       <el-container>
-        <el-aside width="30%" style="padding: 20px">
+        <el-aside width="40%" style="padding: 20px">
           <el-tabs v-model="displayName" type="border-card">
             <el-tab-pane label="内存空间" name="tab_first" style="height: 380px">
-              <el-table :data="mainMemory" border style="width: 100%" height="500">
-                <el-table-column fixed prop="start" label="起址" width="90"></el-table-column>
-                <el-table-column fixed prop="length" label="长度" width="94"></el-table-column>
-                <el-table-column fixed prop="memory_state" label="分配情况" width="90"></el-table-column>
-                <el-table-column fixed prop="memory_PCB" label="进程号" width="90"></el-table-column>
+              <el-table :data="mainMemory" border style="width: 100%" height="380">
+                <el-table-column fixed prop="start" label="起址" width="125" align="center"></el-table-column>
+                <el-table-column prop="length" label="长度" width="130" align="center"></el-table-column>
+                <el-table-column prop="memory_state" label="分配情况" width="130" align="center"></el-table-column>
+                <el-table-column prop="memory_PCB" label="进程号" width="125" align="center"></el-table-column>
               </el-table>
             </el-tab-pane>
-            <el-tab-pane label="处理机" name="tab_second" style="height: 380px">处理机</el-tab-pane>
-            <el-tab-pane label="后备队列" name="tab_third" style="height: 380px">后备队列</el-tab-pane>
+            <el-tab-pane label="处理机" name="tab_second" style="height: 380px">
+              <div v-for="processor in processors" :key="processor.id" style="float: left">
+                <el-table :data="processor.pid_list" border style="width: 100%" height="380">
+                  <el-table-column v-bind:label="processor.id" style="margin-bottom: 10px" align="center">
+                    <el-table-column fixed prop="pid" label="进程号" width="120" align="center"></el-table-column>
+                  </el-table-column>
+                </el-table>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="后备队列" name="tab_third" style="height: 380px">
+              <el-table :data="backupQueue" border style="width: 100%" height="380">
+                <el-table-column fixed prop="pid" label="进程号" align="center"></el-table-column>
+              </el-table>
+            </el-tab-pane>
             <el-tab-pane label="挂起队列" name="tab_forth" style="height: 380px">
-              挂起队列
+              <el-table :data="hangingQueue" border style="width: 100%" height="380">
+                <el-table-column fixed prop="pid" label="进程号" align="center"></el-table-column>
+              </el-table>
             </el-tab-pane>
           </el-tabs>
         </el-aside>
-        <el-main width="70%">
+        <el-main width="60%">
           <div class="demo-fit">
             <div class="block" v-for="fit in fits" :key="fit">
               <span class="title">{{ fit }}</span>
@@ -118,6 +147,9 @@ export default {
       },
       pcbData: [],
       mainMemory: [],
+      processors: [],
+      backupQueue: [],
+      hangingQueue: [],
     };
   },
   computed: {
@@ -136,18 +168,31 @@ export default {
         });
       }
       return options;
+    },
+    pcb_value: function () {
+      var values = [];
+      for (var i = 0; i < this.pcbData.length; i++) {
+        values.push({
+          'key': this.pcbData[i].pid,
+        });
+      }
+      return values;
+    },
+    hangingValue: function () {
+      var values = [];
+      for (var i = 0; i < this.hangingQueue.length; i++) {
+        values.push(this.hangingQueue[i].pid);
+      }
+      return values;
     }
   },
   watch: {
     pcbData: function () {
-      this.$axios({
-        method: "get",
-        url: "http://127.0.0.1:5000/os/get_main_memory", // 接口地址
-      }).then(response => {
-        console.log(response);   // 成功的返回
-        this.mainMemory = this.transferMainMemory(response.data['main_memory']); // 将返回的数据转换为用于在表格中显示的形式
-      }).catch(error => console.log(error, "error")); // 失败的返回
-    }
+      this.getMainMemory();
+      this.getProcessors();
+      this.getBackupQueue();
+      this.getHangingQueue();
+    },
   },
   methods: {
     refresh() {
@@ -162,16 +207,19 @@ export default {
     run() {
       this.$axios({
         method: "get",
-        url: "http://127.0.0.1:5000/os/run", // 接口地址
+        url: "http://127.0.0.1:5000/os/run",
       }).then(response => {
-        console.log(response);   // 成功的返回
-        this.pcbData = this.transferPCB(response.data['pcb_list']); // 将返回的数据转换为用于在表格中显示的形式
-      }).catch(error => console.log(error, "error")); // 失败的返回
+        console.log(response);
+        this.pcbData = this.transferPCB(response.data['pcb_list']);
+      }).catch(error => console.log(error, "error"));
     },
-    submit() {
-      if(this.form.precursor.length === 0) {
+    createPCB() {
+      if (this.form.property === 1 && this.form.precursor.length === 0) {
         this.form.property = 0;
-        alert('未选择前驱进程，已切换为独立进程！');
+        this.$message({
+          message: '未选择前驱进程，已切换为独立进程！',
+          type: 'warning'
+        });
       }
       this.$axios({
         method: "post",
@@ -228,7 +276,7 @@ export default {
     },
     transferMainMemory(main_memory) {
       for (var i = 0; i < main_memory.length; i++) {
-        switch (main_memory[i].memory_state){
+        switch (main_memory[i].memory_state) {
           case 'UNASSIGNED':
             main_memory[i].memory_state = '未分配';
             break;
@@ -241,12 +289,80 @@ export default {
         }
       }
       return main_memory;
+    },
+    getMainMemory() {
+      this.$axios({
+        method: "get",
+        url: "http://127.0.0.1:5000/os/get_main_memory",
+      }).then(response => {
+        console.log(response);
+        this.mainMemory = this.transferMainMemory(response.data['main_memory']);
+      }).catch(error => console.log(error, "error"));
+    },
+    getProcessors() {
+      this.$axios({
+        method: "get",
+        url: "http://127.0.0.1:5000/os/get_processors",
+      }).then(response => {
+        console.log(response);
+        this.processors = response.data['processors'];
+      }).catch(error => console.log(error, "error"));
+    },
+    getBackupQueue() {
+      this.$axios({
+        method: "get",
+        url: "http://127.0.0.1:5000/os/get_backup_queue",
+      }).then(response => {
+        console.log(response);
+        this.backupQueue = response.data['backup_queue'];
+      }).catch(error => console.log(error, "error"));
+    },
+    getHangingQueue() {
+      this.$axios({
+        method: "get",
+        url: "http://127.0.0.1:5000/os/get_hanging_queue",
+      }).then(response => {
+        console.log(response);
+        this.hangingQueue = response.data['hanging_queue'];
+      }).catch(error => console.log(error, "error"));
+    },
+    hangingDataChange(hangingPIDs, direction, movedPIDs) {
+      // hangingPIDList：所有挂起的pid列表
+      // direction：移动方向
+      // movedPIDList：本次移动的pid列表
+      console.log(hangingPIDs, direction, movedPIDs);
+      if (direction === 'right') {
+        this.hangPCBList(movedPIDs);
+      }
+      if (direction === 'left') {
+        this.unhangPCBList(movedPIDs);
+      }
+    },
+    hangPCBList(pcbList) {
+      this.$axios({
+        method: "post",
+        url: "http://127.0.0.1:5000/os/hang_PCB_list",
+        data: {'form': pcbList,}
+      }).then(response => {
+        console.log(response);
+        this.pcbData = this.transferPCB(response.data['pcb_list']);
+      }).catch(error => console.log(error, "error"));
+    },
+    unhangPCBList(pcbList) {
+      this.$axios({
+        method: "post",
+        url: "http://127.0.0.1:5000/os/unhang_PCB_list",
+        data: {'form': pcbList,}
+      }).then(response => {
+        console.log(response);
+        this.pcbData = this.transferPCB(response.data['pcb_list']);
+      }).catch(error => console.log(error, "error"));
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
 .el-header, .el-footer {
   background-color: #B3C0D1;
   color: #333;
@@ -264,5 +380,9 @@ export default {
   background-color: #E9EEF3;
   color: #333;
   text-align: center;
+}
+
+.el-transfer-panel {
+  width: 150px !important;
 }
 </style>
